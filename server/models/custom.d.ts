c3ca81fdb991df5b3c7bd79e5f207e009580dbad
ambiti0n <fabulousfresh010@gmail.com>
2022-03-01 11:40:54 +0100

    profile endpoints

--- server/models/custom.d.ts
+++ server/models/custom.d.ts
@@ -2,5 +2,5 @@ import { IUser } from '../utils/interface';
 import { Request } from 'express';
 
 export interface CustomReq extends Request {
-  user?: IUser;
+  user?: Iuser | any;
 }
--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,12 +1,19 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from "express";
 
-import {signup, login, logout, getAllUser } from './../controllers/authController'
+import { signup, login, logout } from "./../controllers/authController";
+import {
+  getProfile,
+  getUserProfile,
+  getAllUser,
+} from "./../controllers/userController";
 
 const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/login',login);
-router.get('/', getAllUser);
-router.get('/logout', logout);
+router.post("/signup", signup);
+router.post("/login", login);
+router.get("/profile", getProfile);
+router.get("/", getAllUser);
+router.get("/logout", logout);
+router.get("/:userId", getUserProfile);
 
 export default router;
