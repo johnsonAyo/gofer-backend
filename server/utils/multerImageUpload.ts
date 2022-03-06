014f9f7fcb7705cd1c3a5bbaa066639647b8a7a7
ambiti0n <fabulousfresh010@gmail.com>
2022-03-06 23:26:12 +0100

    svg file included in image types

--- server/utils/multerImageUpload.ts
+++ server/utils/multerImageUpload.ts
@@ -1,5 +1,5 @@
-import multer from 'multer';
-import path from 'path';
+import multer from "multer";
+import path from "path";
 
 // const storage = multer.diskStorage({});
 
@@ -10,15 +10,20 @@ import path from 'path';
 const imageMulter = multer({
   storage: multer.diskStorage({
     filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-      cb(null, file.fieldname + '-' + uniqueSuffix);
+      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+      cb(null, file.fieldname + "-" + uniqueSuffix);
     },
   }),
   fileFilter: (req: any, file: any, cb: any) => {
     let extn = path.extname(file.originalname);
 
-    if (extn !== '.jpg' && extn !== '.jpeg' && extn !== '.png') {
-      cb(new Error('File type is not supported...'), false);
+    if (
+      extn !== ".jpg" &&
+      extn !== ".jpeg" &&
+      extn !== ".png" &&
+      extn !== ".svg"
+    ) {
+      cb(new Error("File type is not supported..."), false);
       return;
     }
     cb(null, true);
